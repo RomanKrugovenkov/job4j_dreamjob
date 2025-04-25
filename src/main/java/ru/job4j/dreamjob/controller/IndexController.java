4747267879e80f999efb53836b8e8416bf3690f3
krugo <krugovenkov@mail.ru>
2025-04-25 15:43:04 +0300

    Header и Footer [#505050 #541166]

--- src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -2,12 +2,14 @@ package ru.job4j.dreamjob.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/index")
-    public String getIndex() {
+    @GetMapping("/")
+    public String getIndex(Model model) {
+        model.addAttribute("pageTitle", "Работа мечты!");
         return "index";
     }
 }
